72347d06fe03cea9b307870adc64ffff4a980a68
Demetrius Edwards <edwardsd03@outlook.com>
2025-07-27 23:39:34 -0500

    Updated app

--- ylg-app/src/App.jsx
+++ ylg-app/src/App.jsx
@@ -1,1 +1,29 @@
+import React from 'react';
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+//import './App.css';
 
+function App() {
+
+  return (
+      <div className='App'>
+        <HashRouter>
+          <Header />
+          <main>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/about' element={<About />} />
+              <Route path='/contact' element={<Contact />} />
+            </Routes>
+          </main>
+          <Footer />
+        </HashRouter>
+      </div>
+  )
+}
+
+export default App;
