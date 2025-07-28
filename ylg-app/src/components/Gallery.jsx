bdf14857817c229473de85223cd368c1aef19d27
Demetrius Edwards <edwardsd03@outlook.com>
2025-07-28 02:21:52 -0500

    making chages to fix an issue

--- ylg-app/src/components/Gallery.jsx
+++ ylg-app/src/components/Gallery.jsx
@@ -1,28 +1,36 @@
-import React from 'react';
-import '../stylesheets/Gallery.css';
+import React from "react";
+import "../stylesheets/Gallery.css";
+import Landscape1 from "../assets/Landscape1.jpg";
+import Landscape2 from "../assets/Landscape2.jpg";
+import Landscape3 from "../assets/Landscape3.jpg";
+import Landscape4 from "../assets/Landscape4.jpg";
+import Landscape5 from "../assets/Landscape5.jpg";
+import Landscape6 from "../assets/Landscape6.jpg";
+import Landscape7 from "../assets/Landscape7.jpg";
+import Landscape8 from "../assets/Landscape8.jpg";
 
 function Gallery() {
-    const imageUrls = [
-        '/images/pics/Landscape1.jpg',
-        '/images/pics/Landscape2.jpg',
-        '/images/pics/Landscape3.jpg',
-        '/images/pics/Landscape4.jpg',
-        '/images/pics/Landscape5.jpg',
-        '/images/pics/Landscape6.jpg',
-        '/images/pics/Landscape7.jpg',
-        'images/pics/Landscape8.jpg'
-    ];
-    
-    return (
-        <section className="gallery">
-            <h3>This Could Be Your Lawn</h3>
-            <div className="galleryContainer">
-                {imageUrls.map((url, index) => (
-                    <img key={index} src={url} alt={`Landscape ${index + 1}`} />
-                ))}
-            </div>
-        </section>
-    );
+  const images = [
+    Landscape1,
+    Landscape2,
+    Landscape3,
+    Landscape4,
+    Landscape5,
+    Landscape6,
+    Landscape7,
+    Landscape8,
+  ];
+
+  return (
+    <section className="gallery">
+      <h3>This Could Be Your Lawn</h3>
+      <div className="galleryContainer">
+        {images.map((url, index) => (
+          <img key={index} src={src} alt={`Landscape ${index + 1}`} />
+        ))}
+      </div>
+    </section>
+  );
 }
 
 export default Gallery;
